3e295942c7689d08bae98e79b8f88d1f15c37adf
ANA-C-C <165332658+ANA-C-C@users.noreply.github.com>
2024-05-10 19:05:09 -0300

    Add files via upload

---
+++ CLASE 6/bucles.js
@@ -0,0 +1,29 @@
+for (let i=0; i<5; i+1) {
+    suma=suma+i;
+    console.log('Variable de interaccion', i);
+}
+console.log('Varible suma', suma);
+
+while(suma<3) {
+    suma=suma+1;
+    console.log(suma);
+}
+
+switch (expresion) {
+    case valor1:
+        //Declaracones ejecutdas cuando el resultado de expresion coincide con el valor1
+        [break];
+}
+
+let result = ' ';
+let i =0;
+
+do {
+    i = i+1;
+} while (i<5);
+console.log (result);
+
+continue [etiqueta];
+
+break [etiqueta];
+
---
+++ CLASE 6/veraciad.js
@@ -0,0 +1,13 @@
+Boolean (" ")
+true
+Boolean ("texto")
+false
+Boolean (1);
+Boolean (0);
+Boolean (-1);
+Boolean (true);
+Boolean (false);
+Boolean ('string');
+Boolean (null);
+Boolean (undefined);
+Boolean ([ ]);
